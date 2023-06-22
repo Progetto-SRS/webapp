c8e19cbd46deec8b217fdfd6a0b3164ccc4cea3b
LucaAndreani97 <andreani.luca97@gmail.com>
2023-06-22 13:52:16 +0200

    update vulnerabilities

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 var RateLimit = require('express-rate-limit');
+const helmet = require('helmet')
 
 
 
@@ -45,7 +46,7 @@ var limiter = RateLimit({
 
 
 
-  const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
+/*  const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
 
   app.use(expressCspHeader({
       directives: {
@@ -53,11 +54,35 @@ var limiter = RateLimit({
           'script-src': [SELF, INLINE, 'https://use.fontawesome.com/releases/v6.3.0/js/all.js','https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js','https://use.fontawesome.com/releases/v6.3.0/js/all.js', 'https://cdn.jsdelivr.net/npm/sweetalert2@11'], 
           'style-src': [SELF, INLINE, 'https://fonts.googleapis.com','https://fonts.gstatic.com','https://cdn.jsdelivr.net',],
           'worker-src': [NONE],
+          'img-src': [SELF],
           'block-all-mixed-content': true,
           'font-src' : [SELF, 'https://fonts.googleapis.com','https://fonts.gstatic.com'],
           'connect-src':[SELF, 'https://dev-functions-srs.azurewebsites.net']
       }
-  }));
+  }));*/
+
+    app.use(helmet.frameguard())
+    app.use(helmet())
+
+    app.use(
+        helmet({
+        contentSecurityPolicy: {
+            directives: {
+            "default-src": ["'self'"],
+            "script-src": ["'self'", "https://use.fontawesome.com/releases/v6.3.0/js/all.js", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js", "https://cdn.jsdelivr.net/npm/sweetalert2@11"],
+            "style-src": ["'self'", "'unsafe-hashes'", "https://fonts.googleapis.com/", "https://cdn.jsdelivr.net/", "'sha256-Jc7XaRBVYMy6h6FvjL32miHrOGOxYV+OP4swZ/9Gysw='", "'sha256-W3tWEOJiO3ZTcqvnkpTB5C2PlIns5RvN9Izb9rlyatk='", "'sha256-O/GGR/+AnJgPWWvk5AiFjzoqSwcI2JErAgAg5tDDckI='", "'sha256-R+j6NQzJkNwWKkGgaf5bHf7ahm3K74JapNVlxqCeBYk='", "'sha256-Insxnoa2NVdEsedbK1Vic5YdAwRJL0Z/3mGjQSg8CAg='", "'sha256-O2+tCymNJNXAt1PNeGpUdjx9tYTHtsUHA3yHgvt0o98='", "'sha256-C1Wm2thZBI0ZnFrVeGrUcPYUjGDDKSysR0ReamqR6+o='", "'sha256-z4w21HkwFSjKJ11qtfcblSXcVk5tq8MSm9mptoIdIjo='", "'sha256-6kyk1L/KT1ROqx+M13bVf8aiRv6D/5SWn/9W2F1uiKg='", "'sha256-iycC9+figSze82zUgT8sJhE7Jd36IKLDjNwTVxRFoW0='" 
+                           , "'sha256-b2cYTrXOXPMAX1LENRu9Od4jkDpHrTe/Q1oNTy89rKk='", "'sha256-U6lgpdmyJsluTDoH4kwD0JSWt4D6/aNxbWiSEu1sth0='", "'sha256-DMyIUGEpI2cVY8f6ETa2uyEDZsagiosmJzY/VH73bAA='", "'sha256-SyJySKAQlV+CjUj10UhuDMZkFacN+tMR56xQ1opONvs='", "'sha256-nentKI3AonBil9U9z40gQWY9+GPH6+nYAr9p6EOCfbU='", "'sha256-vEd8tvEGbf045lmYg1z2/HzDFur6PYcB8ktZecS58dw='",
+                        "'sha256-q4EY/AXymGxhYVm0SvOALBywrUMguozfaOWrsdIzjHA='", "'sha256-zHXAmDrNxTAQhDr6lPfPQduSjn2o9s87I4BrhUzdzOk='", "'sha256-pOfy5lld0TbaymjgQBa5+sjHuNbErQdWG+QRWyNOBbc='", "'sha256-38EBcaq+4zBMvV3b/V/OH8kNCe6lPH+GqRwcTS7sIlU='"],
+            "font-src": ["'self'", "https://fonts.googleapis.com/", "https://fonts.gstatic.com/"],
+            "form-action": ["'self'"],
+            //"img-src": ["'self", "http://www.w3.org/2000/svg", ".\public\img"],
+            "connect-src": ["self", "https://dev-functions-srs.azurewebsites.net/"],
+            },
+        },
+        })
+    );
+  
+
 app.use(limiter);
 app.use(morgan('dev')); //combined o common for production
 app.use(bodyParser.urlencoded({extended:true}));
